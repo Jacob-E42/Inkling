8cc49796d087d83f850ac27a1e866b0e59faf055
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-16 14:48:03 -0400

    updated schema

--- backend/inkling-schema.sql
+++ backend/inkling-schema.sql
@@ -2,15 +2,14 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  username TEXT NOT NULL,
   email TEXT NOT NULL,
-  password TEXT NOT NULL,
+  password TEXT NOT NULL CHECK (position('@' IN email) > 1),
   interests JSON
 );
 
 CREATE TABLE journal_entries (
   id SERIAL PRIMARY KEY,
-  user_id INT REFERENCES users(id),
+  user_id INT REFERENCES users(id) ON DELETE CASCADE,
   title TEXT NOT NULL,
   entry_text TEXT NOT NULL,
   entry_date TIMESTAMP NOT NULL,
