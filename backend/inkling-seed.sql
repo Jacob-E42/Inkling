8733238c797785c9d59bc18ba54ebc77568d823e
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-21 16:24:17 -0400

    inkling sql files

---
+++ backend/inkling-seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (first_name, last_name, username, email, password, interests)
+VALUES ('John', 'Doe', 'johndoe@example.com', 'password123', '{"habit building", "daily"}'),
+('Jane', 'Smith',  'janesmith@example.com', 'password456', '{"daily", "gratitude"}'),
+ ('Mike', 'Johnson',  'mikejohnson@example.com', 'password789', '{"self-reflection", "improve writing"}'), 
+ ('Jacob', 'Eiferman',  'jacobeiferman@example.com', '123456789', '{"self-reflection", "stream of consciousness"}')
+ ;
---
+++ backend/inkling.sql
@@ -0,0 +1,18 @@
+\echo 'Delete and recreate inkling db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE inkling;
+CREATE DATABASE inkling;
+\connect inkling
+
+\i inkling-schema.sql
+\i inkling-seed.sql
+
+\echo 'Delete and recreate inkling_test db?'
+\prompt 'Return for yes or control-C to cancel > ' foo
+
+DROP DATABASE inkling_test;
+CREATE DATABASE inkling_test;
+\connect inkling_test
+
+\i inkling-schema.sql
