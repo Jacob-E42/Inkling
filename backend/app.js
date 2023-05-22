e4fb173c2e91f8bdfd3266b6917f5078db4453fb
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-22 12:29:24 -0400

    created app.js

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+/** Express app for Inkling. */
+
+const express = require("express");
+const cors = require("cors");
+
+const { NotFoundError } = require("./expressError");
+
+const { authenticateJWT } = require("./middleware/auth");
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/users");
+const journalRoutes = require("./routes/journals");
+
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(authenticateJWT);
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/journals", journalRoutes);
+
+/** Handle 404 errors -- this matches everything */
+app.use(function (req, res, next) {
+	return next(new NotFoundError());
+});
+
+/** Generic error handler; anything unhandled goes here. */
+app.use(function (err, req, res, next) {
+	if (process.env.NODE_ENV !== "test") console.error(err.stack);
+	const status = err.status || 500;
+	const message = err.message;
+
+	return res.status(status).json({
+		error: { message, status }
+	});
+});
+
+module.exports = app;
