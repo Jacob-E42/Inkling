0a5ea8de3f605516e8e98ec7d9406b35c67e2b42
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-22 13:14:29 -0400

    Homepage component

---
+++ frontend/src/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+	return (
+		<div>
+			<h1>Inkling</h1>
+			<p>Start journaling and unlock your potential</p>
+			<a href="/signup">Sign Up / Log In</a>
+		</div>
+	);
+};
+
+export default Homepage;
