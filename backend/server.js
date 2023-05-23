913a22e0261bfcb38b22a232dbc80b16db2a30d1
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-23 12:57:33 -0400

    added comments

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 const app = require("./app");
 const { PORT } = require("./config");
 
+//start server on port specified in env variable
 app.listen(PORT, function () {
 	console.log(`Started on http://localhost:${PORT}`);
 });
