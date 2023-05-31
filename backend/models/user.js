23224bc885de3a5c66a078b199b32651a0fe1c34
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-31 12:30:33 -0400

    created user model, with register method

--- backend/models/user.js
+++ backend/models/user.js
@@ -11,23 +11,26 @@ class User {
 		this.tableName = "users";
 	}
 
+	// Retrieves a user by their ID
 	async getById(id) {
 		return this.db(this.tableName).where({ id }).first();
 	}
 
+	// Retrieves a user by their email
 	async getByEmail(email) {
 		return this.db(this.tableName).where({ email }).first();
 	}
 
+	// Registers a new user
 	async register(firstName, lastName, email, password, interests) {
 		// Check if user with the same email already exists
 		const existingUser = await this.getByEmail(email);
 		if (existingUser) {
-			throw new Error("User with this email already exists");
+			throw new BadRequestError("User with this email already exists");
 		}
 
 		// Hash the password
-		const hashedPassword = await bcrypt.hash(password, 10);
+		const hashedPassword = await bcrypt.hash(password, bcr);
 
 		// Insert new user into the database
 		const newUser = {
@@ -37,7 +40,9 @@ class User {
 			password: hashedPassword,
 			interests
 		};
-		const result = await this.db(this.tableName).insert(newUser).returning("*");
+		const result = await this.db(this.tableName)
+			.insert(newUser)
+			.returning(["first_name", "last_name", "email", "interests"]);
 
 		// Return the newly created user
 		return result[0];
