f5a61dba8ae1b54f62adc7a6070347f3943a3f9c
Jacob Eiferman <jacobeiferman@gmail.com>
2023-05-31 12:42:11 -0400

    tests for createToken method

---
+++ backend/helpers/token.test.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const { createToken } = require("./token");
+const { SECRET_KEY } = require("../config");
+
+describe("createToken", () => {
+	const user = {
+		email: "test@example.com"
+	};
+
+	it("should create a valid JWT token", () => {
+		const token = createToken(user);
+		expect(token).toBeDefined();
+
+		// Verify the token using the secret key
+		const decoded = jwt.verify(token, SECRET_KEY);
+		expect(decoded.email).toEqual(user.email);
+	});
+
+	it("should throw an error if user data is missing", () => {
+		expect(() => createToken()).toThrowError("User data is missing");
+	});
+});
