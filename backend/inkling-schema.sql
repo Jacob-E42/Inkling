3cc32168420891be755f9b6b84471cf82b99eaba
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-01 11:44:54 -0400

    updated schema

--- backend/inkling-schema.sql
+++ backend/inkling-schema.sql
@@ -2,9 +2,10 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
-  email TEXT NOT NULL,
-  password TEXT NOT NULL CHECK (position('@' IN email) > 1),
-  interests JSON
+  email TEXT NOT NULL CHECK (position('@' IN email) > 1),
+  password TEXT NOT NULL ,
+  interests text[] NOT NULL CHECK (cardinality(interests) > 0)
+
 );
 
 CREATE TABLE journal_entries (
