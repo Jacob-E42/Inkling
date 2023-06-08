5ccd4ce20ddc6eb15954aefdc4039d5039ee5b35
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-08 13:28:58 -0400

    ContextProvider for current user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import Router from "./Router";
 import "./App.css";
+import { UserProvider } from "./UserContext";
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<Router />
-			</header>
-		</div>
+		<UserProvider>
+			<div className="App">
+				<header className="App-header">
+					<Router />
+				</header>
+			</div>
+		</UserProvider>
 	);
 }
 
