d1e5be52b15f1fe067b38a51d0f1f6ef7262d7e0
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-08 13:23:42 -0400

    Homepage component

--- frontend/src/Homepage/Homepage.js
+++ frontend/src/Homepage/Homepage.js
@@ -1,12 +1,28 @@
 import React from "react";
+import { Button, Container } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
 	return (
-		<div>
-			<h1>Inkling</h1>
-			<p>Start journaling and unlock your potential</p>
-			<a href="/signup">Sign Up / Log In</a>
-		</div>
+		<Container>
+			<h1 className="mt-5">Inkling</h1>
+			<p className="lead">A cool slogan here</p>
+			<div className="d-flex justify-content-center">
+				<Button
+					color="primary"
+					className="mr-3"
+					tag={Link}
+					to="/signup">
+					Sign Up
+				</Button>
+				<Button
+					color="secondary"
+					tag={Link}
+					to="/login">
+					Log In
+				</Button>
+			</div>
+		</Container>
 	);
 };
 
