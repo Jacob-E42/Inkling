e3605702de23c2e1101a59f29ddeae94f741a4a6
Jacob Eiferman <jacobeiferman@gmail.com>
2023-06-16 11:32:32 -0400

    signup method

--- frontend/src/UserContext.js
+++ frontend/src/UserContext.js
@@ -1,9 +1,26 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import Request from "./api";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
 	const [user, setUser] = useState(null);
+	const [token, setToken] = useState(null);
 
-	return <UserContext.Provider value={{ user, setUser }}>{children}</UserContext.Provider>;
+	const request = new Request(token);
+
+	useEffect(function loadUserInfo() {}, [token]);
+
+	const signup = formData => {
+		try {
+			const token = request.signup(formData);
+			setToken(token);
+			return { success: true };
+		} catch (errors) {
+			console.error("signup failed", errors);
+			return { success: false, errors };
+		}
+	};
+
+	return <UserContext.Provider value={{ user, setUser, signup }}>{children}</UserContext.Provider>;
 };
