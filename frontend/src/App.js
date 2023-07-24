fb38b56df3501ebdb979e2035356897ba4928abf
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-24 15:17:29 -0400

    added UserContext functions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,94 @@
+import React, { useState, useEffect, useCallback } from "react";
+import useLocalStorage from "./hooks/useLocalStorage";
+import Request from "./api";
+import jwtDecode from "jwt-decode";
 import Router from "./Router";
+import UserContext from "./UserContext";
 import "./App.css";
-import { UserProvider } from "./UserContext";
 
 function App() {
+	const [user, setUser] = useLocalStorage("user", null);
+	const [token, setToken] = useLocalStorage("token", null);
+	const [infoLoaded, setInfoLoaded] = useState(false);
+	const [msg, setMsg] = useState("");
+	const [color, setColor] = useState("primary");
+	console.log(infoLoaded, setInfoLoaded, msg, setMsg, color, setColor);
+	useEffect(
+		function loadUserInfo() {
+			console.debug("App useEffect loadUserInfo", "token=", token);
+
+			async function getCurrentUser() {
+				console.debug("getCurrentUser");
+				if (token) {
+					try {
+						let { email } = await jwtDecode(token);
+						email = await email.trim();
+						console.log("email", email);
+						// put the token on the Api class so it can use it to call the API.
+						const request = new Request(token);
+						const currentUser = await request.getCurrentUser(email);
+						setUser(currentUser);
+						console.log("current user", currentUser);
+					} catch (err) {
+						console.error(err);
+						setUser(null);
+					}
+				} else {
+					console.debug("No Token");
+				}
+			}
+			getCurrentUser();
+		},
+		[token, setUser]
+	);
+
+	const signup = async formData => {
+		console.debug("signup");
+		try {
+			const request = new Request();
+			const token = await request.signup(formData);
+			console.log(token);
+			if (token) setToken(token);
+			else throw new Error("missing sign in token");
+			return { success: true };
+		} catch (errors) {
+			console.error("signup failed", errors);
+			return { success: false, errors };
+		}
+	};
+
+	const login = useCallback(
+		async formData => {
+			console.debug("login");
+			try {
+				const request = new Request();
+				const token = await request.login(formData);
+				if (token) setToken(token);
+				else throw new Error("Log in token in missing");
+				return { success: true };
+			} catch (errors) {
+				console.error("login failed", errors);
+				return { success: false, errors };
+			}
+		},
+		[setToken]
+	);
+
+	/** Handles site-wide logout. */
+	function logout() {
+		console.debug("logout");
+		setUser(null);
+		setToken(null);
+	}
+
 	return (
-		<UserProvider>
+		<UserContext.Provider value={{ user, setUser, signup, login, logout }}>
 			<div className="App">
 				<header className="App-header">
 					<Router />
 				</header>
 			</div>
-		</UserProvider>
+		</UserContext.Provider>
 	);
 }
 
