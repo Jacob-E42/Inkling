ba89b13d1ade3e2ffff49761fb362d81af125ed4
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-24 14:31:48 -0400

    brought in UserContext

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./Homepage/Homepage";
 import LoginForm from "./auth/LoginForm";
 import SignupForm from "./auth/SignupForm";
 import Profile from "./profile/Profile";
+import { UserContext } from "./UserContext";
 
 const AppRouter = () => {
+	const { user } = useContext(UserContext);
+	console.debug("User:", user);
 	return (
 		<Router>
 			<Routes>
