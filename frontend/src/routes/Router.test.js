9ba8ba784e2ce770a8ac96af9a0de961a8ef2b16
Jacob Eiferman <jacobeiferman@gmail.com>
2023-07-28 16:57:11 -0400

    added Router tests

---
+++ frontend/src/routes/Router.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import { AlertProvider, UserProvider } from "../mock";
+import Router from "./Router";
+
+test("Router renders without crashing", () => {
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				{/* <AlertProvider> */}
+				<Router />
+				{/* </AlertProvider> */}
+			</UserProvider>
+		</MemoryRouter>
+	);
+});
+
+test("Router matches snapshot", () => {
+	const { asFragment } = render(
+		<MemoryRouter>
+			<UserProvider>
+				{/* <AlertProvider> */}
+				<Router />
+				{/* </AlertProvider> */}
+			</UserProvider>
+		</MemoryRouter>
+	);
+	expect(asFragment()).toMatchSnapshot();
+});
