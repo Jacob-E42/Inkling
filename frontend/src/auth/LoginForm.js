9d6ef92bfa812d54fe618294285033710bf359de
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-01 12:33:41 -0400

    updated validation method

--- frontend/src/auth/LoginForm.js
+++ frontend/src/auth/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useState } from "react";
-import { Button, Form, FormGroup, Input, Label } from "reactstrap";
+import { Button, Form, FormGroup, Input, Label, FormFeedback } from "reactstrap";
 import { Link, useNavigate } from "react-router-dom";
 import UserContext from "../context_providers/UserContext";
 import useForm from "../hooks/useForm";
@@ -14,60 +14,64 @@ const LoginForm = () => {
 	const [errors, setErrors] = useState({});
 	const navigate = useNavigate();
 
-	const handleSubmit = useCallback(
-		async e => {
-			e.preventDefault();
+	console.debug("LoginForm", "errors=", errors);
 
-			// const validationErrors = validateForm(formData);
-			// if (Object.keys(validationErrors).length === 0) {
-			try {
-				const result = await login(formData);
-				if (result.success) {
-					console.log("Form submitted:", formData);
-					navigate("/profile");
-				}
-			} catch (err) {
-				console.error(err);
-			}
-			// } else {
-			// 	setErrors(validationErrors);
-			// }
-		},
-		[login, formData, navigate]
-	);
-
-	const validateForm = data => {
-		const errors = {};
-
-		if (!data.email.trim()) {
-			errors.email = "Email is required";
-		} else if (!isValidEmail(data.email)) {
-			errors.email = "Invalid email address";
-		}
-
-		if (!data.password.trim()) {
-			errors.password = "Password is required";
-		} else if (!isValidPassword(data.password)) {
-			errors.password =
-				"Password must be at least 8 characters long and contain at least one letter and one number";
-		}
-
-		return errors;
-	};
-
-	const isValidEmail = email => {
+	const isValidEmail = useCallback(email => {
 		// Perform email validation here
 		// You can use regular expressions or other validation libraries
 		// Return true if the email is valid, false otherwise
 		return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
-	};
+	}, []);
 
-	const isValidPassword = password => {
+	const isValidPassword = useCallback(password => {
 		// Perform password validation here
 		// You can use regular expressions or other validation logic
 		// Return true if the password is valid, false otherwise
 		return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d!@#$%^&*]{8,}$/.test(password);
-	};
+	}, []);
+
+	const validateForm = useCallback(
+		async data => {
+			const validationErrors = {};
+
+			if (!data.email.trim()) {
+				validationErrors.email = "Email is required";
+			} else if (!isValidEmail(data.email)) {
+				validationErrors.email = "Invalid email address";
+			}
+
+			if (!data.password.trim()) {
+				validationErrors.password = "Password is required";
+			} else if (!isValidPassword(data.password)) {
+				validationErrors.password =
+					"Password must be at least 8 characters long and contain at least one letter and one number";
+			}
+
+			setErrors(validationErrors); // Updating the state with the constructed errors object
+		},
+		[setErrors, isValidEmail, isValidPassword]
+	);
+
+	const handleSubmit = useCallback(
+		async e => {
+			e.preventDefault();
+			console.debug("handleSubmit");
+			await validateForm(formData);
+			if (Object.keys(errors).length === 0) {
+				try {
+					const result = await login(formData);
+					if (result.success) {
+						console.log("Form submitted:", formData);
+						navigate("/profile");
+						setErrors({});
+					}
+				} catch (err) {
+					console.error(err);
+				}
+			}
+		},
+		[login, formData, navigate, errors, validateForm]
+	);
 
 	return (
 		<Form onSubmit={handleSubmit}>
@@ -83,7 +87,7 @@ const LoginForm = () => {
 					onChange={handleChange}
 					invalid={!!errors.email}
 				/>
-				{errors.email && <div className="error">{errors.email}</div>}
+				<FormFeedback className="inputError">{errors.email}</FormFeedback>
 			</FormGroup>
 			<FormGroup>
 				<Label for="password">Password</Label>
@@ -97,7 +101,7 @@ const LoginForm = () => {
 					onChange={handleChange}
 					invalid={!!errors.password}
 				/>
-				{errors.password && <div className="error">{errors.password}</div>}
+				<FormFeedback className="inputError">{errors.password}</FormFeedback>
 			</FormGroup>
 			<Button type="submit">Submit</Button>
 
