f0a51fd7aa84a0b6389b479115fce6186af3513f
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-04 11:32:14 -0400

    tests for sql helper

---
+++ backend/helpers/sql.test.js
@@ -0,0 +1,27 @@
+const { objectDataToSql } = require("./sql");
+
+describe("objectDataToSql", function () {
+	test("works: 1 item", function () {
+		const result = objectDataToSql({ f1: "v1" });
+		expect(result).toEqual({
+			setCols: '"f1"=$1',
+			values: ["v1"]
+		});
+	});
+
+	test("works: 2 items", function () {
+		const result = objectDataToSql({ f1: "v1", jsF2: "v2" });
+		expect(result).toEqual({
+			setCols: '"f1"=$1, "jsF2"=$2',
+			values: ["v1", "v2"]
+		});
+	});
+
+	test("works: firstName", function () {
+		const result = objectDataToSql({ f1: "v1", firstName: "v2" });
+		expect(result).toEqual({
+			setCols: '"f1"=$1, "first_name"=$2',
+			values: ["v1", "v2"]
+		});
+	});
+});
