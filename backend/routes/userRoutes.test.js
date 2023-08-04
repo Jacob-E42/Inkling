8e32fb8f90cf208a683b733d110c8b7dd2e8a8a8
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-04 11:56:55 -0400

    tests for patch route

--- backend/routes/userRoutes.test.js
+++ backend/routes/userRoutes.test.js
@@ -13,7 +13,8 @@ const {
 	commonAfterAll,
 	u1Token,
 	u2Token,
-	u3Token
+	u3Token,
+	nonUserToken
 } = require("./testUtils.js");
 
 beforeAll(commonBeforeAll);
@@ -219,107 +220,89 @@ describe("GET /users/:email", function () {
 	});
 
 	test("not found if user not found", async function () {
-		const resp = await request(app).get(`/users/nope`).set("authorization", `Bearer ${u1Token}`);
+		const resp = await request(app)
+			.get(`/users/nosuchuser@user.com`)
+			.set("authorization", `Bearer ${nonUserToken}`);
 		expect(resp.statusCode).toEqual(404);
 	});
 });
 
 /************************************** PATCH /users/:username */
 
-// describe("PATCH /users/:username", () => {
-// 	test("works for admins", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/u1`)
-// 			.send({
-// 				firstName: "New"
-// 			})
-// 			.set("authorization", `Bearer ${adminToken}`);
-// 		expect(resp.body).toEqual({
-// 			user: {
-// 				username: "u1",
-// 				firstName: "New",
-// 				lastName: "U1L",
-// 				email: "user1@user.com",
-// 				isAdmin: false
-// 			}
-// 		});
-// 	});
+describe("PATCH /users/:username", () => {
+	test("works for same user", async function () {
+		const resp = await request(app)
+			.patch(`/users/user1@user.com`)
+			.send({
+				firstName: "New"
+			})
+			.set("authorization", `Bearer ${u1Token}`);
+		expect(resp.body).toEqual({
+			user: {
+				firstName: "New",
+				lastName: "U1L",
+				email: "user1@user.com",
+				interests: ["interest1", "interest2"]
+			}
+		});
+	});
 
-// 	test("works for same user", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/u1`)
-// 			.send({
-// 				firstName: "New"
-// 			})
-// 			.set("authorization", `Bearer ${u1Token}`);
-// 		expect(resp.body).toEqual({
-// 			user: {
-// 				username: "u1",
-// 				firstName: "New",
-// 				lastName: "U1L",
-// 				email: "user1@user.com",
-// 				isAdmin: false
-// 			}
-// 		});
-// 	});
+	test("unauth if not same user", async function () {
+		const resp = await request(app)
+			.patch(`/users/user1@user.com`)
+			.send({
+				firstName: "New"
+			})
+			.set("authorization", `Bearer ${u2Token}`);
+		expect(resp.statusCode).toEqual(401);
+	});
 
-// 	test("unauth if not same user", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/u1`)
-// 			.send({
-// 				firstName: "New"
-// 			})
-// 			.set("authorization", `Bearer ${u2Token}`);
-// 		expect(resp.statusCode).toEqual(401);
-// 	});
+	test("unauth for anon", async function () {
+		const resp = await request(app).patch(`/users/user1@user.com`).send({
+			firstName: "New"
+		});
+		expect(resp.statusCode).toEqual(401);
+	});
 
-// 	test("unauth for anon", async function () {
-// 		const resp = await request(app).patch(`/users/u1`).send({
-// 			firstName: "New"
-// 		});
-// 		expect(resp.statusCode).toEqual(401);
-// 	});
+	test("not found if no such user", async function () {
+		const resp = await request(app)
+			.patch(`/users/nosuchuser@user.com`)
+			.send({
+				firstName: "Nope"
+			})
+			.set("authorization", `Bearer ${nonUserToken}`);
+		expect(resp.statusCode).toEqual(404);
+	});
 
-// 	test("not found if no such user", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/nope`)
-// 			.send({
-// 				firstName: "Nope"
-// 			})
-// 			.set("authorization", `Bearer ${adminToken}`);
-// 		expect(resp.statusCode).toEqual(404);
-// 	});
+	test("bad request if invalid data", async function () {
+		const resp = await request(app)
+			.patch(`/users/nosuchuser@user.com`)
+			.send({
+				firstName: 42
+			})
+			.set("authorization", `Bearer ${nonUserToken}`);
+		expect(resp.statusCode).toEqual(400);
+	});
 
-// 	test("bad request if invalid data", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/u1`)
-// 			.send({
-// 				firstName: 42
-// 			})
-// 			.set("authorization", `Bearer ${adminToken}`);
-// 		expect(resp.statusCode).toEqual(400);
-// 	});
-
-// 	test("works: can set new password", async function () {
-// 		const resp = await request(app)
-// 			.patch(`/users/u1`)
-// 			.send({
-// 				password: "new-password"
-// 			})
-// 			.set("authorization", `Bearer ${adminToken}`);
-// 		expect(resp.body).toEqual({
-// 			user: {
-// 				username: "u1",
-// 				firstName: "U1F",
-// 				lastName: "U1L",
-// 				email: "user1@user.com",
-// 				isAdmin: false
-// 			}
-// 		});
-// 		const isSuccessful = await User.authenticate("u1", "new-password");
-// 		expect(isSuccessful).toBeTruthy();
-// 	});
-// });
+	test("works: can set new password", async function () {
+		const resp = await request(app)
+			.patch(`/users/user1@user.com`)
+			.send({
+				password: "new-password"
+			})
+			.set("authorization", `Bearer ${u1Token}`);
+		expect(resp.body).toEqual({
+			user: {
+				firstName: "U1F",
+				lastName: "U1L",
+				email: "user1@user.com",
+				interests: ["interest1", "interest2"]
+			}
+		});
+		const isSuccessful = await User.authenticate("user1@user.com", "new-password");
+		expect(isSuccessful).toBeTruthy();
+	});
+});
 
 /************************************** DELETE /users/:username */
 
