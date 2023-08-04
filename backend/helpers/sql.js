11da192474771fa658c40f0b4ea44420570c8c3a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-04 11:32:06 -0400

    sql helper for partial updates

---
+++ backend/helpers/sql.js
@@ -0,0 +1,54 @@
+const { BadRequestError } = require("../expressError");
+
+/**
+ * Helper for making selective update queries.
+ *
+ * The calling function can use it to make the SET clause of an SQL UPDATE
+ * statement.
+ *
+ * @param dataToUpdate {Object} {field1: newVal, field2: newVal, ...}
+ * @param jsToSql {Object} maps js-style data fields to database column names,
+ *   like { firstName: "first_name", age: "age" }
+ *
+ * @returns {Object} {sqlSetCols, dataToUpdate}
+ *
+ * @example {firstName: 'Aliya', age: 32} =>
+ *   { setCols: '"first_name"=$1, "age"=$2',
+ *     values: ['Aliya', 32] }
+ */
+
+function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+	const keys = Object.keys(dataToUpdate);
+	if (keys.length === 0) throw new BadRequestError("No data");
+
+	// {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+	const cols = keys.map((colName, idx) => `"${jsToSql[colName] || colName}"=$${idx + 1}`);
+
+	return {
+		setCols: cols.join(", "),
+		values: Object.values(dataToUpdate)
+	};
+}
+
+function objectDataToSql(dataToUpdate) {
+	if (dataToUpdate.firstName) {
+		dataToUpdate["first_name"] = dataToUpdate.firstName;
+		delete dataToUpdate.firstName;
+	}
+	if (dataToUpdate.lastName) {
+		dataToUpdate["last_name"] = dataToUpdate.lastName;
+		delete dataToUpdate.lastName;
+	}
+	const keys = Object.keys(dataToUpdate);
+	if (keys.length === 0) throw new BadRequestError("No data");
+
+	// {firstName: 'Aliya', age: 32} => ['"first_name"=$1', '"age"=$2']
+	const cols = keys.map((colName, idx) => `"${colName}"=$${idx + 1}`);
+
+	return {
+		setCols: cols.join(", "),
+		values: Object.values(dataToUpdate)
+	};
+}
+
+module.exports = { sqlForPartialUpdate, objectDataToSql };
