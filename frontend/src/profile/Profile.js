dbb4c6aba38306eac4c8e1fa81e4cbab86c3c4ec
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-07 09:14:44 -0400

    added form and handleSubmit

--- frontend/src/profile/Profile.js
+++ frontend/src/profile/Profile.js
@@ -1,12 +1,40 @@
 import React, { useCallback, useContext } from "react";
-import { Container } from "reactstrap";
+import { Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import { useNavigate } from "react-router-dom";
+import useForm from "../hooks/useForm";
 import UserContext from "../context_providers/UserContext";
+import AlertContext from "../context_providers/AlertContext";
 
 const Profile = () => {
-	const { user, logout } = useContext(UserContext);
+	const { user, logout, updateUser } = useContext(UserContext);
+	const { setMsg, setColor } = useContext(AlertContext);
 	console.debug("Profile", "user=", user);
 	const navigate = useNavigate();
+
+	// State to store form data
+	const [form, handleChange] = useForm({
+		firstName: user.firstName,
+		lastName: user.lastName,
+		email: user.email,
+		interests: user.interests
+	});
+
+	// Handle form submission
+	const handleSubmit = useCallback(
+		async event => {
+			event.preventDefault();
+			try {
+				// Update the current user details
+				await updateUser(form);
+				setMsg("Your information has been updated");
+				setColor("success");
+			} catch (error) {
+				// Log any errors that occur during the update
+				console.error(error);
+			}
+		},
+		[updateUser, setMsg, setColor, form]
+	);
 
 	const handleLogout = useCallback(() => {
 		logout();
@@ -17,6 +45,59 @@ const Profile = () => {
 		<Container>
 			This is profile
 			<p>User: {user ? user.firstName : "No user"}</p>
+			{user && (
+				<Form
+					onSubmit={handleSubmit}
+					className="profile-container">
+					<FormGroup className="profile-item">
+						<Label for="firstName">First Name</Label>
+						<Input
+							type="text"
+							name="firstName"
+							id="firstName"
+							value={form.firstName}
+							onChange={handleChange}
+						/>
+					</FormGroup>
+					<FormGroup className="profile-item">
+						<Label for="lastName">Last Name</Label>
+						<Input
+							type="text"
+							name="lastName"
+							id="lastName"
+							value={form.lastName}
+							onChange={handleChange}
+						/>
+					</FormGroup>
+					<FormGroup className="profile-item">
+						<Label for="email">Email</Label>
+						<Input
+							type="email"
+							name="email"
+							id="email"
+							value={form.email}
+							onChange={handleChange}
+						/>
+					</FormGroup>
+					<FormGroup className="profile-item">
+						<Label for="password">Password</Label>
+						<Input
+							type="password"
+							name="password"
+							id="password"
+							placeholder="Enter new password, if you want it changed"
+							value={form.password}
+							onChange={handleChange}
+						/>
+					</FormGroup>
+
+					<Button
+						type="submit"
+						color="primary">
+						Submit
+					</Button>
+				</Form>
+			)}
 			<button onClick={handleLogout}>Logout</button>
 		</Container>
 	);
