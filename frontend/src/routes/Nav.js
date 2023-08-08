f667f1135e7a94d603e1acc3c040d232007dd159
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-08 10:02:26 -0400

    added Nav

---
+++ frontend/src/routes/Nav.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { NavLink } from "react-router-dom";
+// import { Button } from "reactstrap";
+
+import UserContext from "../context_providers/UserContext";
+import "./Nav.css";
+
+const Nav = ({ logout }) => {
+	const { user } = useContext(UserContext);
+
+	let firstName;
+	if (user) firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
+
+	return (
+		<nav className="nav">
+			<NavLink to="/">Inkling</NavLink>
+			<NavLink to="/journal">Journal</NavLink>
+			{!user && (
+				<>
+					<NavLink to="/login">Login</NavLink>
+					<NavLink to="/signup">Sign Up</NavLink>
+				</>
+			)}
+
+			{user && (
+				<>
+					<NavLink to="/profile">{firstName}</NavLink>
+					<button
+						className="link-button"
+						onClick={logout}>
+						Log Out
+					</button>
+				</>
+			)}
+		</nav>
+	);
+};
+
+export default Nav;
