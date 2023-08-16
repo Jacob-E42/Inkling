91e6acb8e6016d901c720d98a0f6ad36b9cf3599
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-16 13:50:57 -0400

    added loadJournalEntry upon first render

--- frontend/src/journal/JournalEntryPage.js
+++ frontend/src/journal/JournalEntryPage.js
@@ -5,32 +5,63 @@ import Journal from "./Journal";
 // import useForm from "../hooks/useForm";
 // import UserContext from "../context_providers/UserContext";
 import AlertContext from "../context_providers/AlertContext";
+import ApiContext from "../context_providers/ApiContext";
+import UserContext from "../context_providers/UserContext";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const JournalEntryPage = () => {
 	let { date } = useParams();
 	console.debug("JournalEntryPage", "date=", date);
 	const { setMsg, setColor } = useContext(AlertContext);
-	if (!date) {
-		date = new Date();
+	const { user } = useContext(UserContext);
+	const { api } = ApiContext;
+	const [journal, setJournal] = useLocalStorage(null);
 
-		let day = date.getDate();
-		let month = date.getMonth() + 1;
-		let year = date.getFullYear();
+	if (!date || !user) {
+		console.error("A date and a user must be provided");
+		setMsg("A date and a user must be provided");
+		setColor("danger");
+		// date = new Date();
 
-		// This arrangement can be altered based on how we want the date's format to appear.
-		let currentDate = `${year}-${month + 1}-${day}`;
-		console.log(currentDate); // "17-6-2022"
+		// let day = date.getDate();
+		// let month = date.getMonth() + 1;
+		// let year = date.getFullYear();
+
+		// // This arrangement can be altered based on how we want the date's format to appear.
+		// let currentDate = `${year}-${month + 1}-${day}`;
+		// console.log(currentDate); // "17-6-2022"
 	}
 
 	useEffect(() => {
 		setMsg(`Today's date is ${date}`);
 		setColor("primary");
+		loadJournalEntry();
 	});
+
+	const loadJournalEntry = async () => {
+		console.debug("loadJournalEntry");
+		try {
+			const resp = api.getJournalEntryByDate();
+			const { id, userId, entryText, entryDate, title } = resp;
+
+			if (!id || !userId || !entryText || !entryDate || !title) {
+				setMsg("Loading journal failed. Some information is missing");
+				setColor("danger");
+			}
+			setJournal(resp);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
 	return (
 		<>
 			<p>Streak goes here</p>
-			<Journal date={date} />
+			<Journal
+				date={date}
+				title={journal.title}
+				entryText={journal.entryText}
+			/>
 			<p>feedback goes here</p>
 		</>
 	);
