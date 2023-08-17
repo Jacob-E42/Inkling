66a40709c0727c9095b444e800f83f2fd26c509a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-17 11:46:47 -0400

    created Error component

--- frontend/src/common/Error.js
+++ frontend/src/common/Error.js
@@ -1,3 +1,8 @@
 import React from "react";
+import { Alert } from "reactstrap";
 
-const Error = () => {};
+const Error = ({ msg, color }) => {
+	return <Alert color={color}>{msg}</Alert>;
+};
+
+export default Error;
