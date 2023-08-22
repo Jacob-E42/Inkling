8982adf47b085b0008f98d7fece126a08d12c0e7
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-22 12:28:53 -0400

    tests

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -71,6 +71,7 @@ const Router = () => {
 
 	const RedirectToCurrentDateJournal = () => {
 		const currentDate = new Date().toISOString().slice(0, 10); // e.g., "2023-07-25"
+		console.debug(currentDate, typeof currentDate);
 		return (
 			<Navigate
 				to={`/journal/${currentDate}`}
