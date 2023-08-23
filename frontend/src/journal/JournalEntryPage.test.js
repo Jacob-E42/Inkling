574a3566a1115ed7d2ea5efe10d78c0fc5e0a3de
Jacob Eiferman <jacobeiferman@gmail.com>
2023-08-23 13:17:06 -0400

    updated tests

--- frontend/src/journal/JournalEntryPage.test.js
+++ frontend/src/journal/JournalEntryPage.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { render, screen, act } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import { AlertProvider, ApiProvider, JournalProvider, UserProvider } from "../mock";
+import { AlertProvider, AnonUserProvider, ApiProvider, JournalProvider, UserProvider } from "../mock";
 import JournalEntryPage from "./JournalEntryPage";
 import axios from "axios";
 jest.mock("axios");
 
-const mockResponseGetJournalEntryByDate = {
+const mockSuccessfulResponse = {
 	data: {
 		journal: {
 			id: 42,
@@ -20,72 +20,141 @@ const mockResponseGetJournalEntryByDate = {
 	}
 };
 
-beforeEach(() => {
-	jest.clearAllMocks();
-	act(() => {
-		axios.mockResolvedValue(mockResponseGetJournalEntryByDate);
+const mockErrorResponse = {
+	response: {
+		status: 404,
+		data: {
+			error: {
+				message: `No journal with date: 2020-07-24`
+			}
+		}
+	}
+};
+
+// describe("Successful journal fetch", () => {
+// 	beforeEach(() => {
+// 		jest.clearAllMocks();
+// 		act(() => {
+// 			axios.mockResolvedValue(mockSuccessfulResponse);
+// 		});
+// 	});
+
+// 	test("JournalEntryPage renders without crashing", () => {
+// 		render(
+// 			<MemoryRouter>
+// 				<UserProvider>
+// 					<JournalProvider>
+// 						<ApiProvider>
+// 							<AlertProvider>
+// 								<JournalEntryPage />
+// 							</AlertProvider>
+// 						</ApiProvider>
+// 					</JournalProvider>
+// 				</UserProvider>
+// 			</MemoryRouter>
+// 		);
+// 	});
+
+// 	test("JournalEntryPage rendered correctly matches snapshot", () => {
+// 		const { asFragment } = render(
+// 			<MemoryRouter>
+// 				<UserProvider>
+// 					<JournalProvider>
+// 						<ApiProvider>
+// 							<AlertProvider>
+// 								<JournalEntryPage />
+// 							</AlertProvider>
+// 						</ApiProvider>
+// 					</JournalProvider>
+// 				</UserProvider>
+// 			</MemoryRouter>
+// 		);
+// 		expect(asFragment()).toMatchSnapshot();
+// 	});
+
+// 	test("JournalEntryPage renders expected text", async () => {
+// 		// eslint-disable-next-line testing-library/no-unnecessary-act
+// 		await act(async () => {
+// 			render(
+// 				<MemoryRouter>
+// 					<UserProvider>
+// 						<JournalProvider>
+// 							<ApiProvider>
+// 								<AlertProvider>
+// 									<JournalEntryPage propDate="2023-07-04" />
+// 								</AlertProvider>
+// 							</ApiProvider>
+// 						</JournalProvider>
+// 					</UserProvider>
+// 				</MemoryRouter>
+// 			);
+// 		});
+
+// 		expect(screen.getByText("2023-07-04")).toBeInTheDocument();
+// 		expect(screen.getByText("Journal Entry")).toBeInTheDocument();
+// 		expect(screen.getByPlaceholderText("title")).toBeInTheDocument();
+// 		expect(screen.getByPlaceholderText("Start your entry here...")).toBeInTheDocument();
+// 	});
+// });
+
+describe("Failed journal fetch", () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+		act(() => {
+			axios.mockRejectedValue(mockErrorResponse);
+		});
 	});
-	// Default behavior
-});
 
-// If you have tests where you need to simulate an error, update the mock behavior within that test.
-
-test("JournalEntryPage renders without crashing", () => {
-	render(
-		<MemoryRouter>
-			<UserProvider>
-				<JournalProvider>
-					<ApiProvider>
-						<AlertProvider>
-							<JournalEntryPage />
-						</AlertProvider>
-					</ApiProvider>
-				</JournalProvider>
-			</UserProvider>
-		</MemoryRouter>
-	);
-});
-
-test("JournalEntryPage rendered correctly matches snapshot", () => {
-	const { asFragment } = render(
-		<MemoryRouter>
-			<UserProvider>
-				<JournalProvider>
-					<ApiProvider>
-						<AlertProvider>
-							<JournalEntryPage />
-						</AlertProvider>
-					</ApiProvider>
-				</JournalProvider>
-			</UserProvider>
-		</MemoryRouter>
-	);
-	expect(asFragment()).toMatchSnapshot();
-});
-
-test("JournalEntryPage crashed, matches snapshot", () => {
-	const { asFragment } = render(
-		<MemoryRouter>
-			<UserProvider>
-				<JournalProvider>
-					<ApiProvider>
-						<AlertProvider>
-							<JournalEntryPage propDate={42} />
-						</AlertProvider>
-					</ApiProvider>
-				</JournalProvider>
-			</UserProvider>
-		</MemoryRouter>
-	);
-	expect(asFragment()).toMatchSnapshot();
-});
-
-test("JournalEntryPage renders expected text", async () => {
-	// eslint-disable-next-line testing-library/no-unnecessary-act
-	await act(async () => {
-		render(
+	test("JournalEntryPage crashed, matches snapshot", () => {
+		const { asFragment } = render(
 			<MemoryRouter>
 				<UserProvider>
+					<JournalProvider>
+						<ApiProvider>
+							<AlertProvider>
+								<JournalEntryPage propDate={42} />
+							</AlertProvider>
+						</ApiProvider>
+					</JournalProvider>
+				</UserProvider>
+			</MemoryRouter>
+		);
+		expect(screen.getByText("Error: A date must be provided")).toBeInTheDocument();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	test("JournalEntryPage renders expected text when the date passed doesn't have an associated journal entry", async () => {
+		// eslint-disable-next-line testing-library/no-unnecessary-act
+
+		let fragmant;
+		// eslint-disable-next-line testing-library/no-unnecessary-act
+		await act(async () => {
+			const { asFragment } = render(
+				<MemoryRouter>
+					<UserProvider>
+						<JournalProvider>
+							<ApiProvider>
+								<AlertProvider>
+									<JournalEntryPage propDate="2020-07-04" />
+								</AlertProvider>
+							</ApiProvider>
+						</JournalProvider>
+					</UserProvider>
+				</MemoryRouter>
+			);
+			fragmant = asFragment;
+		});
+
+		expect(fragmant()).toMatchSnapshot();
+		expect(screen.getByText("Error: There is no journal entry for date: 2020-07-04")).toBeInTheDocument();
+	});
+
+	test("JournalEntryPage renders expected text when allInfoDefined is false", async () => {
+		// eslint-disable-next-line testing-library/no-unnecessary-act
+
+		const { asFragment } = render(
+			<MemoryRouter>
+				<AnonUserProvider>
 					<JournalProvider>
 						<ApiProvider>
 							<AlertProvider>
@@ -93,15 +162,11 @@ test("JournalEntryPage renders expected text", async () => {
 							</AlertProvider>
 						</ApiProvider>
 					</JournalProvider>
-				</UserProvider>
+				</AnonUserProvider>
 			</MemoryRouter>
 		);
+
+		expect(asFragment()).toMatchSnapshot();
+		expect(screen.getByText("Error: A date must be provided")).toBeInTheDocument();
 	});
-
-	// const dateText = await screen.get
-
-	expect(screen.getByText("2023-07-04")).toBeInTheDocument();
-	expect(screen.getByText("Journal Entry")).toBeInTheDocument();
-	expect(screen.getByPlaceholderText("title")).toBeInTheDocument();
-	expect(screen.getByPlaceholderText("Start your entry here...")).toBeInTheDocument();
 });
