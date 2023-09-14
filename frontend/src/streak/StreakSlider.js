1dc20cab475b34bf16601cb9675a0a2037b7d4b3
Jacob Eiferman <jacobeiferman@gmail.com>
2023-09-14 13:22:00 -0400

    added state to track loading

--- frontend/src/streak/StreakSlider.js
+++ frontend/src/streak/StreakSlider.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useCallback } from "react";
+import React, { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -7,31 +7,22 @@ import DayBlock from "./DayBlock";
 import useLocalStorage from "../hooks/useLocalStorage";
 import useValidateDate from "../hooks/useVerifyDate";
 import { getCurrentDate, getDateRange } from "../common/dateHelpers";
-import { parseISO, isAfter } from "date-fns";
+
 import { useNavigate } from "react-router-dom";
-import AlertContext from "../context_providers/AlertContext";
+import LoadingSpinner from "../common/LoadingSpinner";
 
 const StreakSlider = ({ date }) => {
-	const [datesToRender, setDatesToRender] = useLocalStorage("datesToRender", null);
-
 	const navigate = useNavigate();
 	if (!useValidateDate(date)) navigate(`journal/${getCurrentDate()}`);
-
-	const settings = {
-		accessability: true,
-		arrows: true,
-		dots: true,
-		infinite: false,
-		speed: 500,
-		slidesToShow: 7,
-		slidesToScroll: 3
-	};
+	const [datesToRender, setDatesToRender] = useLocalStorage("datesToRender", [date]);
+	const [slidesLoaded, setSlidesLoaded] = useLocalStorage("slidesLoaded", false);
 
 	useEffect(() => {
 		setDatesToRender(getDateRange(date));
-	}, [date, setDatesToRender]);
+		setSlidesLoaded(true);
+	}, [date, setDatesToRender, setSlidesLoaded]);
 
-	const dayBlocks = datesToRender.map((day, index) => {
+	const daySlides = datesToRender.map((day, index) => {
 		return (
 			<DayBlock
 				day={day}
@@ -39,12 +30,29 @@ const StreakSlider = ({ date }) => {
 			/>
 		);
 	});
-	console.log(datesToRender, dayBlocks);
+	console.log(datesToRender, datesToRender.length, typeof daySlides);
+
+	if (!slidesLoaded) return <LoadingSpinner />;
+
+	const totalSlides = daySlides.length;
+	const settings = {
+		accessability: true,
+		arrows: true,
+		centerMode: false,
+		className: "Slider",
+		dots: true,
+		infinite: false,
+		initialSlide: datesToRender.length - 1,
+		slide: "div",
+		speed: 500,
+		slidesToShow: process.env.NODE_ENV === "test" ? totalSlides : 10,
+		slidesToScroll: 3
+	};
 
 	return (
 		<div className="StreakSlider">
 			<h2> Streak </h2>
-			<Slider {...settings}>{dayBlocks}</Slider>
+			<Slider {...settings}>{daySlides}</Slider>
 		</div>
 	);
 };
