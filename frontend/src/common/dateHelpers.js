9969f73afa45a858548f4751acaf69a540356496
Jacob Eiferman <jacobeiferman@gmail.com>
2023-09-14 11:16:30 -0400

    added new helper functions

--- frontend/src/common/dateHelpers.js
+++ frontend/src/common/dateHelpers.js
@@ -1,20 +1,77 @@
+import {
+	eachDayOfInterval,
+	format,
+	parse,
+	parseISO,
+	subDays,
+	getDay,
+	differenceInCalendarDays,
+	addDays
+} from "date-fns";
+
+// Return today's date in format "yyyy-mm-dd"
 function getCurrentDate() {
-	const currentDate = new Date().toISOString().slice(0, 10); // e.g., "2023-07-25"
+	const currentDate = format(new Date(), "yyyy-MM-dd", { weekStartsOn: 0 });
 	return currentDate;
 }
 
-const getPreviousTenDays = dateString => {
-	let date = new Date(dateString);
-	let datesArray = [];
+//Return an array of Dates corresponding to the difference between the calendar day of dateString and the date
+// that's num days before the dateString.
+// The date passed in is included as the last element in the array. Therefore the number of returned elements is n + 1.
+//
+//dateString has format: "yyyy-mm-dd"
+const getPreviousNumDays = (dateString, num) => {
+	const date = parseISO(dateString);
+	let days = eachDayOfInterval({ start: subDays(date, num), end: date });
+	const daysStrings = days.map(date => {
+		return format(date, "yyyy-MM-dd");
+	});
+	return daysStrings;
+};
+//Return an array of Dates corresponding to the difference between the calendar day of dateString and the date
+// that's num days after the dateString.
+//dateString has format: "yyyy-mm-dd"
+const getNextNumDays = (dateString, num) => {
+	if (num === 0) return [];
+	const date = parseISO(dateString);
+	console.log(dateString, num);
+	let days = eachDayOfInterval({ start: addDays(date, 1), end: addDays(date, num) });
+	console.log(days);
+	const daysStrings = days.map(date => {
+		return format(date, "yyyy-MM-dd");
+	});
 
-	for (let i = 0; i < 10; i++) {
-		date.setDate(date.getDate() - 1); // Subtract 1 day from the current date
-		datesArray.push(date.toISOString().slice(0, 10)); // Format to 'YYYY-MM-DD'
-	}
-
-	return datesArray;
+	return daysStrings;
 };
 
-export { getPreviousTenDays };
+//Return a string corresponding to the day of the week of the dateString
+//dateString has format: "yyyy-mm-dd"
+const getDayOfWeek = dateString => {
+	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+	const date = parseISO(dateString);
 
-export default getCurrentDate;
+	// const day = days[getDay(date)];
+	// console.log(day, getDay(date));
+	return days[getDay(date)];
+};
+
+//get the range of dateStrings that the StreakSlider should render
+// accepts the dateString of the day whose journal will be shown
+function getDateRange(dateString) {
+	const date = parseISO(dateString);
+	const currentDate = parseISO(getCurrentDate());
+
+	const difference = differenceInCalendarDays(currentDate, date);
+	if (difference < 0) return new RangeError("The date cannot be in the future!");
+	const prevDays = getPreviousNumDays(dateString, 30);
+	const nextDays = difference <= 30 ? getNextNumDays(dateString, difference) : getNextNumDays(dateString, 30);
+
+	const dateRange = [...prevDays, ...nextDays];
+	// for (let date of dateRange) {
+	// 	date = format(date, "yyyy-MM-dd", { weekStartsOn: 0 });
+	// }
+	console.log("difference=", difference, "prevDays=", prevDays, "nextDays=", nextDays);
+	return dateRange;
+}
+
+export { getPreviousNumDays, getDayOfWeek, getCurrentDate, getNextNumDays, getDateRange };
