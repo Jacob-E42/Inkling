e9397e818c4cf5bede96fc3cb6cfda05dbce21bb
Jacob Eiferman <jacobeiferman@gmail.com>
2023-09-19 15:19:08 -0400

    renaming

---
+++ frontend/src/streak/StreakDisplay.js
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+// import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./Streak.css";
+// import DayBlock from "./DayBlock";
+import useLocalStorage from "../hooks/useLocalStorage";
+import useValidateDate from "../hooks/useVerifyDate";
+import { getCurrentDate, getDateRange, getDayOfWeek } from "../common/dateHelpers";
+import { format, parseISO } from "date-fns";
+import { useNavigate, Link } from "react-router-dom";
+import LoadingSpinner from "../common/LoadingSpinner";
+
+const StreakDisplay = ({ date }) => {
+	console.debug("StreakDisplay", date);
+	const navigate = useNavigate();
+	if (!useValidateDate(date)) navigate(`journal/${getCurrentDate()}`);
+	const [daysArray, setDaysArray] = useLocalStorage("datesToRender", [date]);
+	const [slidesLoaded, setSlidesLoaded] = useLocalStorage("slidesLoaded", false);
+	// const startDate = daysArray[0];
+	const endDate = daysArray[daysArray.length - 1];
+	// const daysArray = eachDayOfInterval({ start: new Date(startDate), end: new Date(endDate) }).reverse(); // reverse to have endDate at one end
+	useEffect(() => {
+		setDaysArray(getDateRange(date));
+		setSlidesLoaded(true);
+	}, [date, setDaysArray, setSlidesLoaded]);
+
+	// const daySlides = datesToRender.map((day, index) => {
+	// 	return (
+	// 		<DayBlock
+	// 			day={day}
+	// 			key={index}
+	// 		/>
+	// 	);
+	// });
+	// console.log(datesToRender, datesToRender.length, typeof daySlides);
+
+	if (!slidesLoaded) return <LoadingSpinner />;
+
+	// const totalSlides = datesToRender.length;
+
+	// const settings = {
+	// 	accessability: true,
+	// 	arrows: true,
+	// 	centerMode: false,
+	// 	className: "Slider",
+	// 	dots: true,
+	// 	infinite: false,
+	// 	initialSlide: datesToRender.length - 1,
+	// 	slide: "div",
+	// 	speed: 500,
+	// 	slidesToShow: process.env.NODE_ENV === "test" ? totalSlides : 10,
+	// 	slidesToScroll: 3
+	// };
+
+	return (
+		<div className="streak-container">
+			<h2> Streak </h2>
+			<br />
+			{daysArray.map(date => (
+				<Link
+					key={date}
+					to={`/journal/${date}`}
+					className={`day-link ${date === endDate ? "active" : ""}`}>
+					{getDayOfWeek(date)}
+					<br />
+					{format(parseISO(date), "dd")}
+				</Link>
+			))}
+		</div>
+	);
+
+	// return (
+	// 	<div className="StreakDisplay">
+
+	// 		<Slider {...settings}>{daySlides}</Slider>
+	// 	</div>
+	// );
+};
+
+export default StreakDisplay;
---
+++ frontend/src/streak/StreakDisplay.test.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { AlertProvider, UserProvider } from "../mock";
+import StreakDisplay from "./StreakDisplay";
+import { getCurrentDate, getPastDate } from "../common/dateHelpers";
+import { parseISO, subDays } from "date-fns";
+
+test("StreakDisplay renders without crashing", () => {
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				<AlertProvider>
+					<StreakDisplay date={getCurrentDate()} />
+				</AlertProvider>
+			</UserProvider>
+		</MemoryRouter>
+	);
+});
+
+test("StreakDisplay matches snapshot", () => {
+	const { asFragment } = render(
+		<MemoryRouter>
+			<UserProvider>
+				<AlertProvider>
+					<StreakDisplay date={getCurrentDate()} />
+				</AlertProvider>
+			</UserProvider>
+		</MemoryRouter>
+	);
+	expect(asFragment()).toMatchSnapshot();
+});
+
+test("StreakDisplay renders 30 days when date is today", () => {
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				<AlertProvider>
+					<StreakDisplay date={getCurrentDate()} />
+				</AlertProvider>
+			</UserProvider>
+		</MemoryRouter>
+	);
+
+	const allDaysRendered = screen.getAllByRole("link");
+	// console.log(allDaysRendered);
+	expect(allDaysRendered.length).toBe(31);
+});
+
+test("StreakDisplay renders at least 60 days when date is over a month ago", () => {
+	const today = getCurrentDate();
+	const pastDate = getPastDate(today, 45);
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				<AlertProvider>
+					<StreakDisplay date={pastDate} />
+				</AlertProvider>
+			</UserProvider>
+		</MemoryRouter>
+	);
+
+	const allDaysRendered = screen.getAllByRole("link");
+	console.log(allDaysRendered.length);
+	expect(allDaysRendered.length).toBeGreaterThan(60);
+});
+
+test("StreakDisplay renders correct amount of days when date is less than a month ago", () => {
+	const today = getCurrentDate();
+	const pastDate = getPastDate(today, 15);
+	render(
+		<MemoryRouter>
+			<UserProvider>
+				<AlertProvider>
+					<StreakDisplay date={pastDate} />
+				</AlertProvider>
+			</UserProvider>
+		</MemoryRouter>
+	);
+
+	const allDaysRendered = screen.getAllByRole("link");
+	console.log(allDaysRendered.length);
+	expect(allDaysRendered.length).toBe(46);
+});
