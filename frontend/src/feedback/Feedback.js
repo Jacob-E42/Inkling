142c5e820ee200f294978db4c4f465c5659e81b7
Jacob Eiferman <jacobeiferman@gmail.com>
2023-09-26 11:37:25 -0400

    new files

---
+++ frontend/src/feedback/Feedback.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+// import AlertContext from "../context_providers/AlertContext";
+import "./Feedback.css";
+
+const Feedback = () => {
+	return (
+		<>
+			<p>feedback goes here</p>
+		</>
+	);
+};
+
+export default Feedback;
