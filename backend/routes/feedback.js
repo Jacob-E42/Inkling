cce33952f8ea7438fea0795b5950a759e014e5e0
Jacob Eiferman <jacobeiferman@gmail.com>
2023-09-27 10:46:49 -0400

    routes for requests to openai

---
+++ backend/routes/feedback.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+const jsonschema = require("jsonschema");
+const receiveFeedbackSchema = require("../schema/receiveFeedbackSchema.json");
+const { BadRequestError, NotFoundError } = require("../expressError");
+const { generatePrompt } = require("../feedbackAPI/promptGenerator");
+const {
+	ensureCorrectUser,
+	ensureCorrectUserByEmail,
+	ensureLoggedIn,
+	ensureCorrectUserByUserId
+} = require("../middleware/authMiddleware");
+
+router.post("/", ensureLoggedIn, async function (req, res, next) {
+	console.debug("POST /feedback/ ");
+
+	try {
+		const validator = jsonschema.validate(req.body, receiveFeedbackSchema);
+		if (!validator.valid) {
+			const errs = validator.errors.map(e => e.stack);
+			throw new BadRequestError(errs);
+		}
+		// if (req.params.userId !== String(req.body.userId)) {
+		// 	throw new NotFoundError("The user id provided doesn't match any known user");
+		// }
+		//desctructure for readability
+
+		const feedback = generatePrompt(req.body.entryText);
+		return res.json({ feedback });
+	} catch (err) {
+		return next(err);
+	}
+});
+
+module.exports = router;
