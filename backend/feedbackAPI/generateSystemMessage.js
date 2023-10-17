99a8c09c9f504d20e3b1e4e6c3b7d5baa4be3e1a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-10-17 15:08:16 -0400

    helper function to design input messages

---
+++ backend/feedbackAPI/generateSystemMessage.js
@@ -0,0 +1,6 @@
+function generateSystemMessage() {
+	return `You are a helpful assistant giving feedback on a user's journal entry. The user is someone who is 	
+	new to the practice of journaling and is trying to build a habit. `;
+}
+
+module.exports = { generateSystemMessage };
