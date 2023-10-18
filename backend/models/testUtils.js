08b30d1da05cf3fb33c93609dbcebe1c34e944c5
Jacob Eiferman <jacobeiferman@gmail.com>
2023-10-18 11:37:30 -0400

    update test entries

--- backend/models/testUtils.js
+++ backend/models/testUtils.js
@@ -14,29 +14,38 @@ async function commonBeforeAll() {
 	await db.query("SELECT setval('users_id_seq', 1, false)");
 	await db.query("SELECT setval('journal_entries_id_seq', 1, false)");
 
-	await User.register("U1F", "U1L", "user1@user.com", "password1", ["interest1", "interest2"]);
-	await User.register("U2F", "U2L", "user2@user.com", "password2", ["interest2", "interest3"]);
-	await User.register("U3F", "U3L", "user3@user.com", "password3", ["interest1", "interest3"]);
-	await User.register("testfirst", "testlast", "test@test.net", "zoomfoog42", ["baking", "jetskiing"]);
+	await User.register("U1F", "U1L", "user1@user.com", "password1");
+	await User.register("U2F", "U2L", "user2@user.com", "password2");
+	await User.register("U3F", "U3L", "user3@user.com", "password3");
+	await User.register("testfirst", "testlast", "test@test.net", "zoomfoog42");
 
-	await Journal.createEntry(1, "My birthday", "Today was my birthday and I had a great day.", "2022-01-04");
+	await Journal.createEntry(
+		1,
+		"My birthday",
+		"Today was my birthday and I had a great day.",
+		"2022-01-04",
+		'["Gratitude Journaling"]'
+	);
 	await Journal.createEntry(
 		2,
 		"How to be more grateful",
 		"I would like to start being more grateful for the little things in life. As part of that I will try to appreciate whatever nice things happen to me unexpectedly.",
-		"2023-01-04"
+		"2023-01-04",
+		'["Gratitude Journaling"]'
 	);
 	await Journal.createEntry(
 		3,
 		"Habits",
 		"Today I did a third of my prescribed habits. Hopefully tomorrow I'll hit the ground running and do better.",
-		"2024-01-04"
+		"2024-01-04",
+		'["Gratitude Journaling"]'
 	);
 	await Journal.createEntry(
 		4,
 		"Front end",
 		"This is a test for the front end, it should be visible there",
-		"2023-01-04"
+		"2023-01-04",
+		'["Gratitude Journaling"]'
 	);
 }
 
