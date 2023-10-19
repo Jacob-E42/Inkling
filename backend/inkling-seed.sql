54bdec5a0acc02c5e3b5c9017b064419a44eed99
Jacob Eiferman <jacobeiferman@gmail.com>
2023-10-19 09:58:25 -0400

    fix small error

--- backend/inkling-seed.sql
+++ backend/inkling-seed.sql
@@ -8,7 +8,7 @@ VALUES
 
 INSERT INTO journal_entries (user_id, title, entry_text, entry_date, emotions, journal_type)
 VALUES
-  (1, 'First Entry', 'Lorem ipsum dolor sit amet.', '2023-01-01', '{"emotions":"joy"}', '["Self-reflection", "Habit tracking"]'),
-  (1, 'Second Entry', 'Consectetur adipiscing elit.', '2023-01-02', '{"emotions":"sadness"}', '["Creative writing", "Personal growth"]'),
-  (2, 'Third Entry', 'Sed do eiusmod tempor incididunt.', '2023-01-03', '{"emotions":"anger"}', '["Goal setting", "Gratitude"]'),
-  (3, 'Fourth Entry', 'Ut enim ad minim veniam.', '2023-01-04', '{"emotions":"fear"}', '["Personal growth, Self-reflection"]');
+  (1, 'First Entry', 'Lorem ipsum dolor sit amet.', '2023-01-01', '{"emotions":"joy"}', '{"Self-reflection", "Habit tracking"}'),
+  (1, 'Second Entry', 'Consectetur adipiscing elit.', '2023-01-02', '{"emotions":"sadness"}', '{"Creative writing", "Personal growth"}'),
+  (2, 'Third Entry', 'Sed do eiusmod tempor incididunt.', '2023-01-03', '{"emotions":"anger"}', '{"Goal setting", "Gratitude"}'),
+  (3, 'Fourth Entry', 'Ut enim ad minim veniam.', '2023-01-04', '{"emotions":"fear"}', '{"Personal growth, Self-reflection"}');
