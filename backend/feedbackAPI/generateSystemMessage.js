2fc4387925485f87c20375dfd78888152151f890
Jacob Eiferman <jacobeiferman@gmail.com>
2023-10-20 17:29:13 -0400

    shorten prompts

--- backend/feedbackAPI/generateSystemMessage.js
+++ backend/feedbackAPI/generateSystemMessage.js
@@ -1,17 +1,16 @@
 function generateSystemMessage(journalType) {
 	let message = `You are a helpful assistant giving feedback on a user's journal entry. The user is someone who is new to the practice of journaling and is trying to build a habit. Help the user be motivated and to align to the type of journaling that they're trying to do. `;
 	if (!journalType) return null;
-	if (journalType === "Dream Journal") message += `Dream Journaling. The user keep a dream journal of their dreams. `;
+	message += `This is the user's journal type and a short description of it. ${journalType}`;
+	if (journalType === "Dream Journal") message += `The user keep a dream journal of their dreams. `;
 	if (journalType === "Gratitude Journal")
-		message += `Gratitude Journaling. The user practices reflecting and recording things they are grateful for. `;
-	if (journalType === "Daily Journal")
-		message += `Daily Journal. Help the user keep a daily log of the day's events.`;
+		message += `The user practices reflecting and recording things they are grateful for. `;
+	if (journalType === "Daily Journal") message += `Help the user keep a daily log of the day's events.`;
 	if (journalType === "Stream-of-Consciousness Journal")
-		message += `Stream of Consciousness Journaling. The user will try to write their thoughts, feelings and impressions as they come to mind.`;
+		message += `The user will try to write their thoughts, feelings and impressions as they come to mind.`;
 	if (journalType === "Reflective Journal")
-		message += `Reflective Journaling. The user records and analyzes personal thoughts, feelings and experiences to learn and grow from past experiences, identify important learning events and gain insight into your inner life`;
-	if (journalType === "Bullet Journal")
-		message += `Bullet Journaling. The user will try to implement the bullet journal technique.`;
+		message += `The user records and analyzes personal thoughts, feelings and experiences to learn and grow from past experiences, identify important learning events and gain insight into your inner life`;
+	if (journalType === "Bullet Journal") message += `The user will try to implement the bullet journal technique.`;
 
 	message += `Please be friendly and encouraging.`;
 	console.log("systemMessage", message, message.length);
