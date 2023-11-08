537c2ae1ea1a000da4af9b8c8091f42335567e4f
Jacob Eiferman <jacobeiferman@gmail.com>
2023-11-08 11:11:11 -0500

    cosmetic change

--- frontend/src/ApiError.js
+++ frontend/src/ApiError.js
@@ -1,6 +1,7 @@
 export default class ApiError extends Error {
 	constructor(message, status) {
 		super(message); // Call the constructor of the Error class
+		this.message = message;
 		this.status = status;
 		this.name = "ApiError"; // This can be useful for error handling later on
 	}
