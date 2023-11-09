539991af9df44676793dddcfdff304ac0f126ccc
Jacob Eiferman <jacobeiferman@gmail.com>
2023-11-09 10:40:52 -0500

    update imports

--- frontend/src/streak/StreakDisplay.js
+++ frontend/src/streak/StreakDisplay.js
@@ -1,9 +1,4 @@
 import React, { useEffect } from "react";
-// import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import "./Streak.css";
-// import DayBlock from "./DayBlock";
 import useLocalStorage from "../hooks/useLocalStorage";
 import useValidateDate from "../hooks/useVerifyDate";
 import { getCurrentDate, getDateRange, getDayOfWeek } from "../common/dateHelpers";
