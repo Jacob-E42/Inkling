864961695bb9e677ab03941ba03bc6d96506e563
Jacob Eiferman <jacobeiferman@gmail.com>
2023-12-02 21:55:47 -0500

    refactor Nav ui

--- frontend/src/routes/Nav.js
+++ frontend/src/routes/Nav.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-// import { Button } from "reactstrap";
-
+import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material/";
+import InklingLogo from "./inkling.png";
 import UserContext from "../context_providers/UserContext";
 import "./Nav.css";
 
@@ -12,29 +12,59 @@ const Nav = ({ logout }) => {
 	if (user) firstName = user.firstName.charAt(0).toUpperCase() + user.firstName.slice(1);
 
 	return (
-		<nav className="nav">
-			<NavLink to="/">Inkling</NavLink>
+		<AppBar position="static">
+			<Toolbar style={{ justifyContent: "space-between" }}>
+				<IconButton
+					edge="start"
+					color="inherit"
+					aria-label="inkling-logo">
+					<img
+						src={InklingLogo}
+						width={100}
+						height={"auto"}
+						alt="Inkling Logo"
+					/>
+				</IconButton>
 
-			{!user && (
-				<>
-					<NavLink to="/login">Login</NavLink>
-					<NavLink to="/signup">Sign Up</NavLink>
-				</>
-			)}
+				<NavLink
+					to="/journal"
+					style={{ textDecoration: "none", color: "inherit" }}>
+					<Typography
+						variant="h6"
+						style={{ textAlign: "center" }}>
+						Journal
+					</Typography>
+				</NavLink>
 
-			{user && (
-				<>
-					<NavLink to="/journal">Journal</NavLink>
-					<NavLink to="/profile">{firstName}</NavLink>
-
-					<button
-						className="link-button"
-						onClick={logout}>
-						Log Out
-					</button>
-				</>
-			)}
-		</nav>
+				{!user ? (
+					<>
+						<NavLink
+							color="inherit"
+							to={"/login"}>
+							<Button color="inherit">Login</Button>
+						</NavLink>
+						<NavLink
+							to={"/signup"}
+							color="inherit">
+							<Button color="inherit">Signup</Button>
+						</NavLink>
+					</>
+				) : (
+					<>
+						<NavLink
+							to="/profile"
+							style={{ textDecoration: "none", color: "inherit" }}>
+							<Button color="inherit">{firstName}</Button>
+						</NavLink>
+						<Button
+							color="inherit"
+							onClick={logout}>
+							Log Out
+						</Button>
+					</>
+				)}
+			</Toolbar>
+		</AppBar>
 	);
 };
 
