f0db8ea3cfdbff6d266656a4a9b782e0847f707a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-12-08 11:34:03 -0500

    create preset for h6

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import App from "./App";
