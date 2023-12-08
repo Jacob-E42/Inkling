41a488aa15990835f3b8462a963ea3948f33b235
Jacob Eiferman <jacobeiferman@gmail.com>
2023-12-08 12:47:49 -0500

    refactor styles

--- frontend/src/streak/StreakDisplay.js
+++ frontend/src/streak/StreakDisplay.js
@@ -6,7 +6,6 @@ import { getCurrentDate, getDateRange, getDayOfWeek, isCurrentDate } from "../co
 import { format, parseISO } from "date-fns";
 import { useNavigate, Link } from "react-router-dom";
 import LoadingSpinner from "../common/LoadingSpinner";
-import "./Streak.css";
 
 const StreakDisplay = ({ date }) => {
 	// console.debug("StreakDisplay", date);
@@ -23,6 +22,7 @@ const StreakDisplay = ({ date }) => {
 			currentDayRef.current.scrollIntoView({ behavior: "auto", block: "nearest", inline: "start" });
 		}
 	}, [date]);
+
 	useEffect(() => {
 		setDaysArray(getDateRange(date));
 		setSlidesLoaded(true);
@@ -31,18 +31,43 @@ const StreakDisplay = ({ date }) => {
 	if (!slidesLoaded) return <LoadingSpinner />;
 
 	return (
-		<div className="streak-container">
+		<div
+			style={{
+				display: "flex",
+				overflowX: "auto",
+				gap: "20px",
+				padding: "10px 0"
+			}}>
 			{daysArray.map(day => (
 				<Link
 					key={day}
 					to={`/journal/${day}`}
-					className={`day-link ${day === date ? "current-day" : ""} ${
-						isCurrentDate(day) ? "today" : `${day} not-today`
-					}`}
+					// className={`day-link ${day === date ? "current-day" : ""} ${
+					// 	isCurrentDate(day) ? "today" : `${day} not-today`
+					// }`}
+					style={{
+						textDecoration: "none",
+						boxShadow: isCurrentDate(day) ? "0px 0px 5px 0px rgba(10, 7, 4, 0.5)" : "none",
+						// backgroundColor: day === date ? "ffaa4c" : "9a9395"
+						backgroundColor: isCurrentDate(day) ? "#9a9395" : "inherit"
+					}}
 					ref={day === getCurrentDate() ? currentDayRef : null}>
 					<Button
 						variant="contained"
-						className={`day-button ${day === date ? "current-day" : "inactive-day"}`}>
+						className={`day-button ${day === date ? "current-day" : "inactive-day"}`}
+						sx={{
+							"display": "flex",
+							"flexDirection": "column",
+							"alignItems": "center",
+							"justifyContent": "center",
+							"borderRadius": "10px",
+							"padding": "10px",
+							"minWidth": "60px",
+							"backgroundColor": day === date ? "#ffaa4c" : "#57a0d3", // Bright Yellow for current day, Carolina Blue otherwise
+							"&:hover": {
+								backgroundColor: "#FFAA4C" // Bright Yellow on hover
+							}
+						}}>
 						<Typography variant="caption">{getDayOfWeek(day)}</Typography>
 						<Typography variant="h6">{format(parseISO(day), "dd")}</Typography>
 					</Button>
