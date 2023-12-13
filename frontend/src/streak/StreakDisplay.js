484a9ee237f426908a6403f4740b6bd154025f3a
Jacob Eiferman <jacobeiferman@gmail.com>
2023-12-13 10:09:50 -0500

    create function to load slides

--- frontend/src/streak/StreakDisplay.js
+++ frontend/src/streak/StreakDisplay.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { Button, Typography } from "@mui/material";
 import useLocalStorage from "../hooks/useLocalStorage";
 import useValidateDate from "../hooks/useVerifyDate";
@@ -6,28 +6,52 @@ import { getCurrentDate, getDateRange, getDayOfWeek, isCurrentDate } from "../co
 import { format, parseISO } from "date-fns";
 import { useNavigate, Link } from "react-router-dom";
 import LoadingSpinner from "../common/LoadingSpinner";
+import { validateDateUserIdAndApi } from "../common/validations";
 
-const StreakDisplay = ({ date }) => {
-	// console.debug("StreakDisplay", date);
+const StreakDisplay = ({ date, userId, api }) => {
+	// console.debug("StreakDisplay", date, userId, api);
+	const allInfoPresent = validateDateUserIdAndApi(date, userId, api);
 	const currentDayRef = useRef(null);
 	const navigate = useNavigate();
+
 	if (!useValidateDate(date)) navigate(`journal/${getCurrentDate()}`);
-	const [daysArray, setDaysArray] = useLocalStorage("datesToRender", [date]);
+
+	const [daysArray, setDaysArray] = useLocalStorage("datesToRender", [{ date, isJournal: true }]);
 	const [slidesLoaded, setSlidesLoaded] = useLocalStorage("slidesLoaded", false);
-	// const startDate = daysArray[0];
-	// const endDate = daysArray[daysArray.length - 1];
-	// const daysArray = eachDayOfInterval({ start: new Date(startDate), end: new Date(endDate) }).reverse(); // reverse to have endDate at one end
+
 	useEffect(() => {
 		if (currentDayRef.current) {
 			currentDayRef.current.scrollIntoView({ behavior: "auto", block: "nearest", inline: "start" });
 		}
-	}, [date]);
+	}, [date, setSlidesLoaded]);
+
+	const loadSlides = useCallback(async () => {
+		console.debug("loadSlides");
+		const dateRange = getDateRange(date);
+
+		const dates = await api.quickCheckJournalEntriesBatch(userId, dateRange);
+		// console.debug("Dates:", dates);
+		setDaysArray(dates);
+		// console.log(daysArray.length, dateRange.length, dates.length);
+		setSlidesLoaded(true);
+	}, [api, date, userId, setDaysArray, setSlidesLoaded]);
 
 	useEffect(() => {
-		setDaysArray(getDateRange(date));
-		setSlidesLoaded(true);
-	}, [date, setDaysArray, setSlidesLoaded]);
+		setSlidesLoaded(false);
+		loadSlides();
+	}, [date, userId, api, loadSlides]);
 
+	// const doDatesHaveEntries = datesArray => {
+	// 	const datesHaveEntries = [];
+	// 	for (let date in datesArray) {
+	// 		const isJournalEntry = api.quickCheckJournalEntry(userId, date);
+	// 		datesHaveEntries.push({ date, isJournal: isJournalEntry });
+	// 	}
+	// 	return datesHaveEntries;
+	// };
+
+	if (!allInfoPresent) return null;
+	console.log(date, userId, api, slidesLoaded, daysArray);
 	if (!slidesLoaded) return <LoadingSpinner />;
 
 	return (
@@ -38,41 +62,57 @@ const StreakDisplay = ({ date }) => {
 				gap: "20px",
 				padding: "10px 0"
 			}}>
-			{daysArray.map(day => (
-				<Link
-					key={day}
-					to={`/journal/${day}`}
-					// className={`day-link ${day === date ? "current-day" : ""} ${
-					// 	isCurrentDate(day) ? "today" : `${day} not-today`
-					// }`}
-					style={{
-						textDecoration: "none",
-						boxShadow: isCurrentDate(day) ? "0px 0px 5px 0px rgba(10, 7, 4, 0.5)" : "none",
-						// backgroundColor: day === date ? "ffaa4c" : "9a9395"
-						backgroundColor: isCurrentDate(day) ? "#9a9395" : "inherit"
-					}}
-					ref={day === getCurrentDate() ? currentDayRef : null}>
-					<Button
-						variant="contained"
-						className={`day-button ${day === date ? "current-day" : "inactive-day"}`}
-						sx={{
-							"display": "flex",
-							"flexDirection": "column",
-							"alignItems": "center",
-							"justifyContent": "center",
-							"borderRadius": "10px",
-							"padding": "10px",
-							"minWidth": "60px",
-							"backgroundColor": day === date ? "#ffaa4c" : "#57a0d3", // Bright Yellow for current day, Carolina Blue otherwise
-							"&:hover": {
-								backgroundColor: "#FFAA4C" // Bright Yellow on hover
-							}
+			{daysArray.map(({ date, isJournal }) =>
+				isJournal ? (
+					<Link
+						key={date}
+						to={`/journal/${date}`}
+						style={{
+							textDecoration: "none",
+							boxShadow: isCurrentDate(date) ? "0px 0px 5px 0px rgba(10, 7, 4, 0.5)" : "none",
+							backgroundColor: isCurrentDate(date) ? "#9a9395" : "inherit"
+						}}
+						ref={date === getCurrentDate() ? currentDayRef : null}>
+						<Button
+							variant="contained"
+							className={`day-button ${date === date ? "current-day" : "inactive-day"}`}
+							sx={{
+								"display": "flex",
+								"flexDirection": "column",
+								"alignItems": "center",
+								"justifyContent": "center",
+								"borderRadius": "10px",
+								"padding": "10px",
+								"minWidth": "60px",
+								"backgroundColor": date === date ? "#ffaa4c" : "#57a0d3",
+								"&:hover": {
+									backgroundColor: "#FFAA4C"
+								}
+							}}>
+							<Typography variant="caption">{getDayOfWeek(date)}</Typography>
+							<Typography variant="h6">{format(parseISO(date), "dd")}</Typography>
+						</Button>
+					</Link>
+				) : (
+					<div
+						key={date}
+						style={{
+							textDecoration: "none",
+							boxShadow: "none",
+							backgroundColor: "#b5b9ac", // Greyed out background for inactive days
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "center",
+							justifyContent: "center",
+							borderRadius: "10px",
+							padding: "10px",
+							minWidth: "60px"
 						}}>
-						<Typography variant="caption">{getDayOfWeek(day)}</Typography>
-						<Typography variant="h6">{format(parseISO(day), "dd")}</Typography>
-					</Button>
-				</Link>
-			))}
+						<Typography variant="caption">{getDayOfWeek(date)}</Typography>
+						<Typography variant="h6">{format(parseISO(date), "dd")}</Typography>
+					</div>
+				)
+			)}
 		</div>
 	);
 };
