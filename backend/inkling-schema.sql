e82c0a78bd8154f13c0f127b09b5433f6f57da80
Jacob Eiferman <jacobeiferman@gmail.com>
2024-01-03 10:00:16 -0500

    add created_at timestamp to db schema

--- backend/inkling-schema.sql
+++ backend/inkling-schema.sql
@@ -3,7 +3,8 @@ CREATE TABLE users (
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   email TEXT NOT NULL CHECK (position('@' IN email) > 1) UNIQUE,
-  password TEXT NOT NULL 
+  password TEXT NOT NULL, 
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE journal_entries (
